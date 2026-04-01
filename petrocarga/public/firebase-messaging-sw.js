ecc59b310dedfd79a7f43ee5b2569c03564134af
IgorAPSantos <igorppandre@hotmail.com>
2026-04-01 18:57:49 -0300

    fix: removendo credenciais explícitas

--- petrocarga/public/firebase-messaging-sw.js
+++ petrocarga/public/firebase-messaging-sw.js
@@ -6,12 +6,13 @@ importScripts(
 );
 
 firebase.initializeApp({
-  apiKey: "AIzaSyDjkIW9FSXzaz2DxaSh7xdwnccZSsya5dw",
-  authDomain: "fir-cptrans-project.firebaseapp.com",
-  projectId: "fir-cptrans-project",
-  storageBucket: "fir-cptrans-project.firebasestorage.app",
-  messagingSenderId: "663684137039",
-  appId: "1:663684137039:web:e724c72ca710c84939aa3a"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || '',
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || '',
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || '',
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || '',
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || '',
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || '',
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID || ''
 });
 
 const messaging = firebase.messaging();
